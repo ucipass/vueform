a071bc7013bdb3c37323628548fe59c1752838b8
ucipass@gmail.com <ucipass@gmail.com>
2021-04-29 09:35:47 -0500

    change

--- src/components/FormPage.vue
+++ src/components/FormPage.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="container">
 
+<!-- NAVBAR -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark pb-0">
     <div class="container-fluid">
       <ul class="navbar-nav">
@@ -24,26 +25,49 @@
     </div>
   </nav>
 
-  <div v-if="input_active">
+<!-- INPUT -->
+  <div v-if="input_active" class="mb-1">
+
     <div v-if="!(inputs.length)" class="jumbotron float-center" >
       <p>Form configuration is not available</p>
     </div>
-    <div v-if="inputs.length">
-      <div v-for="input in inputs" :key="input.id" class="mb-1">
-        <label class="mb-0" :for="input.id">{{input.label}}</label>
-        <input :type="input.type" class="form-control" :id="input.id" :placeholder="input.placeholder" v-model="input.value">
-      </div>
-      <div class="mb-1">
+
+    <div v-if="inputs.length" >
+
+          
+              <div v-for="input in inputs" :key="input.id" class="row d-flex m-2 justify-content-end">
+                <div class="d-flex  justify-content-end">
+                  <label class="m-1" :for="input.id">{{input.label}}</label>
+                  <span class="m-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                      <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                    </svg>                
+                  </span>
+                </div>
+                <div class="d-flex col-9">
+                    <input :type="input.type" class="form-control" :id="input.id" :placeholder="input.placeholder" v-model="input.value">
+                </div>
+              </div>
+          
+          
+
+
+
+      <div class="mb-1 row justify-content-end">
         <button type="button" class="btn btn-outline-primary" @click="submitFn">{{submit.text}}</button>
       </div>
+    
     </div>    
+
   </div>
 
+<!-- OUTPUT -->
   <div v-if="!input_active" class="mb-1">
     <label class="mb-0" for="TextAreaOutput.id">output</label>
     <textarea class="form-control" id="TextAreaOutput" rows="20" :value="JSON.stringify(output, null, 2)"></textarea>
   </div>
-
+  
 </div>
 </template>
 
