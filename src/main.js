ea4374748864a7c2848e41631785f041f78dd786
ucipass@gmail.com <ucipass@gmail.com>
2021-04-30 14:07:00 -0500

    Tooltip and flex changes

--- src/main.js
+++ src/main.js
@@ -2,9 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import $ from 'jquery'
+
 export const eventBus = new Vue();
 
 Vue.config.productionTip = false
+Vue.directive('tooltip', function(el, binding){
+  $(el).tooltip({
+           title: binding.value,
+           placement: binding.arg,
+           trigger: 'hover'             
+       })
+})
 
 new Vue({
   render: h => h(App),
